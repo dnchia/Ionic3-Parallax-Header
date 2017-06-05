29bb42d452918f0ed9ed7a46ec391d284d899188
dnchia <daniel.chia.95@gmail.com>
2017-06-05 10:52:11 +0200

    Elastic-header directive done. Implements a functional parallax header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ElasticHeaderDirective } from '../directives/elastic-header/elastic-header';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ElasticHeaderDirective
   ],
   imports: [
     BrowserModule,
